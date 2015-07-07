2e41d75c78a560826d31876a5ff755f88772501e
dalemy <dalemy@microsoft.com>
2015-07-07 13:29:22 -0700

    Initial Commit

---
+++ CocoaTime/CocoaTime.h
@@ -0,0 +1,13 @@
+//
+//  CocoaTime.h
+//  CocoaTime
+//
+//  Created by Dale Myers on 7/7/15.
+//  Copyright (c) 2015 Dale Myers. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CocoaTime : NSObject
+
+@end
---
+++ CocoaTime/CocoaTime.m
@@ -0,0 +1,13 @@
+//
+//  CocoaTime.m
+//  CocoaTime
+//
+//  Created by Dale Myers on 7/7/15.
+//  Copyright (c) 2015 Dale Myers. All rights reserved.
+//
+
+#import "CocoaTime.h"
+
+@implementation CocoaTime
+
+@end
