a33bf15a2ace81e98e32d289dccfb785bdab1a9c
Vel0x <DaleMyers19@gmail.com>
2015-07-11 23:59:41 -0700

    Added logging to test

--- Example/Tests/BlockTests.m
+++ Example/Tests/BlockTests.m
@@ -26,28 +26,34 @@
     [super tearDown];
 }
 
-- (void)testStandard {
+- (void)testStandard
+{
+	double sleepTime = 2.0;
     for (int i = 0; i < 10; i++)
     {
         NSString *timername = [NSString stringWithFormat:@"Test_%d",i];
         [DMTime startTimer:timername];
-        sleep(1.0);
+        sleep(sleepTime);
         [DMTime endTimer:timername];
         DMTimeResult *result = [DMTime lastResultForKey:timername];
         
         // I need to find something more accurate than sleep(). It seems to be
         // really bad at these kinds of values.
+		NSLog(@"Checking result. Expected: %.2f seconds. Actual: %.2f", sleepTime, [result seconds]);
         XCTAssert([result seconds] > 0.85 && [result seconds] < 1.15, @"Pass");
     }
 }
 
-- (void)testBlocks {
+- (void)testBlocks
+{
+	double sleepTime = 2.0;
     for (int i = 0; i < 10; i++)
     {
         DMTimeResult *result = [DMTime timeBlock:^{
-            sleep(1.0);
+            sleep(sleepTime);
         }];
-        XCTAssert([result seconds] > 0.9 && [result seconds] < 1.1, @"Pass");
+		NSLog(@"Checking result. Expected: %.2f seconds. Actual: %.2f", sleepTime, [result seconds]);
+        XCTAssert([result seconds] > 0.85 && [result seconds] < 1.15, @"Pass");
     }
 }
 
