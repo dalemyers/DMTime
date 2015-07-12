5ff8017c211a1de577006a345a3f0029c0e84383
Vel0x <DaleMyers19@gmail.com>
2015-07-11 23:44:53 -0700

    Removed files from git

--- Example/Pods/Target Support Files/Pods-DMTime_Tests-DMTime/Pods-DMTime_Tests-DMTime-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_DMTime_Tests_DMTime : NSObject
-@end
-@implementation PodsDummy_Pods_DMTime_Tests_DMTime
-@end
--- Example/Pods/Target Support Files/Pods-DMTime_Tests/Pods-DMTime_Tests-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_DMTime_TestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_DMTime_TestsVersionString[];
-
